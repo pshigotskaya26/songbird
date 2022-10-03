a4c5a23b51ea2a99058ca74adf70b90129ae3269
Tanya Pshihotskaya <pshihotskaya.tanya@gmail.com>
2022-10-03 22:33:43 +0300

    feat: do adaptive pick-feed section in donate page

---
+++ online-zoo/pages/donate/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('resize',function(){
+    location.reload();  
+});
+
+let widthWindow = window.innerWidth;
+
+let arrayInputsOfProgressBar = document.querySelectorAll('.progress-bar__input');
+
+
+if (widthWindow <= 700) {
+    arrayInputsOfProgressBar[5].checked = "checked";
+}
